450187fa8d3d94b75addd2e66ce3d21fc0085919
Constantinos Georgiou <constantgeorgiou@gmail.com>
2020-09-13 16:55:31 +0300

    init commit

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Helloooooo</h1>
+                <p>is this working</p>
+            </div>
+        );
+    }
+}
+
+export default App;
